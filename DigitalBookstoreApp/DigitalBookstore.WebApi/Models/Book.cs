77dd58674f325271af12d77ebb524d920eba14ef
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-10-26 23:52:03 -0500

    First commit, added the Controllers and Models folders, created the conection chain

---
+++ DigitalBookstoreApp/DigitalBookstore.WebApi/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace DigitalBookstore.WebApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Yearpublication { get; set; }
+        public User User { get; set; }
+
+    }
+}
---
+++ DigitalBookstoreApp/DigitalBookstore.WebApi/Program.cs
@@ -0,0 +1,35 @@
+using DigitalBookstore.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DBookstoreAppDbContext>(Options =>
+{
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();//2.con esta línea se utilizan rutas???????
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+}
+);
+
+app.UseHttpsRedirection();
+
+app.Run();
